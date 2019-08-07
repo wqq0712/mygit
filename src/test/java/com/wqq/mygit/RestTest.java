a1901c3b9c79d0b550eaf3fae6a5593294218293
wangqq <272334814@qq.com>
2019-08-07 14:04:38 +0800

    修改JGitTest类内容，初始化RestTest类

--- src/test/java/com/wqq/mygit/RestTest.java
+++ src/test/java/com/wqq/mygit/RestTest.java
@@ -1,19 +1,18 @@
 /*
- * @File JGitTest.java
+ * @File RestTest.java
  * @Package com.wqq.mygit
  * @Copyinght Copyright (c) 2019
  * @Company morelean
  * @author Administrator
- * @date 2019年8月7日 上午11:20:03
+ * @date 2019年8月7日 下午1:46:42
  * @version V1.0
  */
 
 package com.wqq.mygit;
 
-public class JGitTest
+public class RestTest
 {
-  //wangqq增加了main方法
   public static void main(String args[]) {
-    System.out.println("增加了main方法");
+    System.out.println("test..");
   }
 }
