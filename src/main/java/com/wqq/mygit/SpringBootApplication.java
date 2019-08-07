025e2232049bfd412ee18227b5e711a438a7e9a6
wangqq <272334814@qq.com>
2019-08-07 20:48:41 +0800

    增加SpringBootApplication类

---
+++ src/main/java/com/wqq/mygit/SpringBootApplication.java
@@ -0,0 +1,18 @@
+/*
+ * @File SpringBootApplication.java
+ * @Package com.wqq.mygit
+ * @Copyinght Copyright (c) 2019
+ * @Company morelean
+ * @author Administrator
+ * @date 2019年8月7日 下午7:31:52
+ * @version V1.0
+ */
+
+package com.wqq.mygit;
+
+public class SpringBootApplication
+{
+  public static void main(String args[]) {
+    System.out.println("spring boot start...");
+  }
+}
