bf691c4f6544a403c8bd5d96bbaa5fc256b015d7
wangqq <272334814@qq.com>
2019-08-07 11:20:32 +0800

    初始化我呢间

---
+++ src/test/java/com/wqq/mygit/JGitTest.java
@@ -0,0 +1,16 @@
+/*
+ * @File JGitTest.java
+ * @Package com.wqq.mygit
+ * @Copyinght Copyright (c) 2019
+ * @Company morelean
+ * @author Administrator
+ * @date 2019年8月7日 上午11:20:03
+ * @version V1.0
+ */
+
+package com.wqq.mygit;
+
+public class JGitTest
+{
+  //添加注释
+}
