9a49bb3c1135573d19c1e5f377473d544d189754
wangqq <272334814@qq.com>
2019-08-07 20:59:40 +0800

    修改了App.java，增加了AppBoot

--- src/main/java/com/wqq/mygit/App.java
+++ src/main/java/com/wqq/mygit/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        //增加了一些内容
+        System.out.println( "haha" );
     }
 }
